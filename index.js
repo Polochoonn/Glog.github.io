7a9cb229f8860a79f1ee1d9ea5adeb656d629ac2
Paul Sastourné <paul@MacBook-Air-de-Paul.local>
2022-11-14 14:45:54 +0100

    javascript fichier

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 $(function() {
     let element = $('#container-01');
-    let config = { backgroundColor: 'orange' };
+    let config = { backgroundColor: 'white' };
     let viewer = $3Dmol.createViewer( element, config );
-    viewer.addSphere({ center: {x:0, y:0, z:0}, radius: 10.0, color: 'green' });
-    viewer.zoomTo();
-    viewer.render();
-    viewer.zoom(0.8, 2000);
-  });
+  let pdbUri = './4qw0.pdb';
+  jQuery.ajax( pdbUri, { 
+    success: function(data) {
+      let v = viewer;
+      v.addModel( data, "pdb" );                       /* load data */
+      v.setStyle({}, {cartoon: {color: 'spectrum'}});  /* style all atoms */
+      v.zoomTo();                                      /* set camera */
+      v.render();                                      /* render scene */
+      v.zoom(1.2, 1000);                               /* slight zoom */
+    },
+    error: function(hdr, status, err) {
+      console.error( "Failed to load PDB " + pdbUri + ": " + err );
+    },
+})
+});
