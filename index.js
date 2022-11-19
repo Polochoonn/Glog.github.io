185b1b0c8634d53adfb5ff4e768658907fe4aeac
Oceane Doremus <oceane.doremus.pro@gmail.com>
2022-11-19 17:17:37 +0100

    Test url

--- index.js
+++ index.js
@@ -1,19 +1,117 @@
-$(function() {
-    let element = $('#container-01');
-    let config = { backgroundColor: 'white' };
-    let viewer = $3Dmol.createViewer( element, config );
-  let pdbUri = './4qw0.pdb';
-  jQuery.ajax( pdbUri, { 
-    success: function(data) {
-      let v = viewer;
-      v.addModel( data, "pdb" );                       /* load data */
-      v.setStyle({}, {cartoon: {color: 'spectrum'}});  /* style all atoms */
-      v.zoomTo();                                      /* set camera */
-      v.render();                                      /* render scene */
-      v.zoom(1.2, 1000);                               /* slight zoom */
-    },
-    error: function(hdr, status, err) {
-      console.error( "Failed to load PDB " + pdbUri + ": " + err );
-    },
-})
-});
+// Fonction de extend pour la navbar
+
+window.onload = function(){
+  let header=
+  document.querySelector("#header");
+  header.innerHTML = `
+  <nav class="navbar navbar-expand-sm navbar-light" id="neubar">
+      <div class="container">
+        <a class="navbar-brand" href="/"
+          ><img
+            src="https://cdn-icons-png.flaticon.com/512/3024/3024310.png"
+            height="60"
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a
+                class="nav-link mx-2 active"
+                aria-current="page"
+                href="/"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link mx-2"
+                href="/chargement.html"
+                >Importation</a
+              >
+            </li>
+            <li class="nav-item">
+              <a 
+                  class="nav-link mx-2" 
+                  href="/visualisation.html">Visualisation</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link mx-2 dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Test
+              </a>
+              <ul
+                class="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li><a class="dropdown-item" href="#">Action1</a></li>
+                <li><a class="dropdown-item" href="#">Action2</a></li>
+                <li><a class="dropdown-item" href="#">Action3</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+`
+}
+
+
+
+// Fonction de redirection 
+// pas utilse elle sert d'exemple pour l'instant
+function RedirectionHome(){
+  document.location.href="./index.html";
+}
+
+
+
+
+
+
+
+// Pour le drag and drop fichier en cours...
+function dropHandler(ev) {
+  console.log('File(s) dropped');
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    [...ev.dataTransfer.items].forEach((item, i) => {
+      // If dropped items aren't files, reject them
+      if (item.kind === 'file') {
+        const file = item.getAsFile();
+        console.log(`… file[${i}].name = ${file.name}`);
+      }
+    });
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    [...ev.dataTransfer.files].forEach((file, i) => {
+      console.log(`… file[${i}].name = ${file.name}`);
+    });
+  }
+}
+
+function dragOverHandler(ev) {
+  console.log('File(s) in drop zone');
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+}
--- initialize_prot.js
+++ initialize_prot.js
@@ -1,8 +1,8 @@
 
-function initialize_4qwo(){
+async function initialize_4qwo(){
 let protein_4qw0 = new Protein("4qWo");
-protein_4qw0.set_pdb("/4qw0.pdb");
-protein_4qw0.set_fasta("./rcsb_pdb_4QWO.fasta");
-console.log(protein_4qw0);
+let test = await fetch("https://files.rcsb.org/view/4QWO.pdb").then(r=> r.text());
+protein_4qw0.set_pdb(test);
+protein_4qw0.set_fasta("/rcsb_pdb_4QWO.fasta");
 return protein_4qw0;
 }
