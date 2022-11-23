2abd039955e815ee97ad19bf198bdb788ba1733a
OceaneDoremus <oceane.doremus.pro@gmail.com>
2022-11-23 15:10:48 +0100

    test du viewer

--- initialize_prot.js
+++ initialize_prot.js
@@ -32,13 +32,17 @@ class Protein {
 
 
 function test(){
+    console.log("test");
     var file = document.getElementById('upload_pdb').files[0];
     var reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
         pdb_protein = initialize_4qwo(reader.result);
-        display3D(pdb_protein);  
+        let a = display3D(pdb_protein);  
+        console.log("bbbbb",a);
+        return a;
     }
+    
 
 // la faire pour voir le fichier pdb
 //     if (file) {
@@ -66,10 +70,9 @@ function test(){
 // alert(pdb_file);
 
 
-function initialize_4qwo(a){
+function initialize_4qwo(current_protein){
     let protein_4qw0 = new Protein("molecule");
-    protein_4qw0.set_pdb(a);
+    protein_4qw0.set_pdb(current_protein);
     protein_4qw0.set_fasta("./rcsb_pdb_4QWO.fasta");
-    console.log(protein_4qw0);
     return protein_4qw0;
 }
--- visualisation.js
+++ visualisation.js
@@ -1,36 +1,47 @@
-    function display3D(protein) {
+function display3D(protein) {
 
-        let element = $('#container-01');
-        let config = { backgroundColor: ' #fdf0d5' };
-        let viewer = $3Dmol.createViewer( element, config );
-        let pdbUri = protein.get_pdb();
-      console.log("displau",pdbUri);
-      jQuery.ajax( pdbUri, { 
-        success: function(data) {
-          let v = viewer;
-          v.addModel( data, "pdb" );                       /* load data */
-          v.setStyle({}, {cartoon: {color: 'spectrum'}});  /* style all atoms */
-          v.zoomTo();                                      /* set camera */
-          v.render();                                      /* render scene */
-          v.zoom(1.2, 1000);                               /* slight zoom */
-        },
-        error: function(hdr, status, err) {
-          console.error( "Failed to load PDB " + pdbUri + ": " + err );
-        },
-    })
+  let element = $('#container-01');
+  let config = { backgroundColor: ' #fdf0d5' };
+  let viewer = $3Dmol.createViewer(element, config);
+  let pdbUri = protein.get_pdb();
+  
+  jQuery.ajax(pdbUri, {
+    success: function (data) {
+      let v = viewer;
+      v.addModel(data, "pdb");                       /* load data */
+      v.setStyle({}, { stick: { color: 'spectrum' } });  /* style all atoms */
+      v.zoomTo();                                      /* set camera */
+      v.render();                                      /* render scene */
+      v.zoom(1.2, 1000);
+      console.log('vvvv', v);
+      return v;
+    },
+    error: function (hdr, status, err) {
+      console.error("Failed to load PDB " + pdbUri + ": " + err);
+    },
+
+  }
+
+  )
+
+
+}
+function modify_viewer(viewer) {
+
+  viewer.setStyle({}, { cartoon: { colorscheme: 'yellowCarbon' }, line: { colorscheme: 'yellowCarbon' } });
+  viewer.render()
 }
 
-
-function visualisation(molecule){
-    console.log(molecule);
-    let test=
+function visualisation(molecule) {
+  console.log(molecule);
+  let test =
     document.querySelector("#molecule");
-    test.innerHTML = `
+  test.innerHTML = `
     <iframe id="inlineFrameExample"
     title="Visualisation proteine connus"
     src="https://3Dmol.org/viewer.html?pdb=${molecule}&style=cartoon:color~spectrum">
 </iframe>
   `
-  }
+}
 
 
