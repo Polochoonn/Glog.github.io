32d4de58ca61b0c2f860c92fd29600f44d7834c3
Paul Sastourné <paul@Air-de-Paul.lan>
2022-11-25 21:25:46 +0100

    enfin

--- visualisation.js
+++ visualisation.js
@@ -148,6 +148,7 @@ function visualisation_nous(){
     document.querySelector("#molecule2");
     test.innerHTML = `
       <h1>Outils</h1>
+      <br>
       <input type="button" value="Stick" onclick="glviewer.setStyle({},{stick:{}}); glviewer.render();">
       <input type="button" value="Line" onclick="glviewer.setStyle({},{line:{}}); glviewer.render();">
       <input type="button" value="Cross" onclick="glviewer.setStyle({},{cross:{linewidth:2}}); glviewer.render();">
