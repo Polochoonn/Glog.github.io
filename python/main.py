34b73f6abfa1ff5742c72affc6406820003d5ca7
Louis Montagne <102073161+louis-montagne@users.noreply.github.com>
2022-12-03 17:56:09 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ from colab import *
 import sys
 
 
-if__name__ = __main__ :
+if__name__ = "__main__" :
   sequence = fastaParser(sys.argv[1])
   test = input(sequence,sys.argv[2])
   sys.stdout.write(test)
