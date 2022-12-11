27b00a0bc98de6457b3a3196a1f2e2e81a51d5fb
OceaneDoremus <106671879+OceaneDoremus@users.noreply.github.com>
2022-12-11 05:57:14 +0100

    Update visualisation.js

--- javascript/visualisation.js
+++ javascript/visualisation.js
@@ -1,141 +1,114 @@
 var glviewer = null;
 
 
-var addLabels = function() {
+var addLabels = function (typeOfAtom) {
+  var labels = [];
   var atoms = glviewer.getModel().selectedAtoms({
-    atom : "CA"
+    atom: typeOfAtom
   });
-  for ( var a in atoms) {
+  for (var a in atoms) {
     var atom = atoms[a];
 
     var l = glviewer.addLabel(atom.resn + " " + atom.resi, {
-      inFront : true,
-      fontSize : 12,
-      position : {
-        x : atom.x,
-        y : atom.y,
-        z : atom.z
-      }
+      inFront: false,
+      fontSize: 12,
+      position: {
+        x: atom.x,
+        y: atom.y,
+        z: atom.z
+      },
+      noshow: true,
+
     });
     atom.label = l;
     labels.push(atom);
   }
 };
 
-var colorSS = function(viewer) {
+
+var removeLabels = function (typeOfAtom) {
+  var atoms = glviewer.getModel().selectedAtoms({
+    atom: typeOfAtom
+  });
+  for (var a in atoms) {
+    var atom = atoms[a];
+    atom.label
+    var l = glviewer.removeLabel(atom.label);
+  }
+};
+
+
+var colorSS = function (viewer) {
   //color by secondary structure
   var m = viewer.getModel();
-  m.setColorByFunction({}, function(atom) {
-    if(atom.ss == 'h') return "magenta";
-    else if(atom.ss == 's') return "orange";
+  m.setColorByFunction({}, function (atom) {
+    if (atom.ss == 'h') return "magenta";
+    else if (atom.ss == 's') return "orange";
     else return "white";
-    
+
   }); viewer.render();
 }
 
-var colorSpectrum = function(viewer) {
-  //color by secondary structure
+var colorSpectrum = function (viewer) {
   var m = viewer.getModel();
   m.setStyle({}, { cartoon: { color: 'spectrum' } });
   viewer.render();
 }
 
-var atomcallback = function(atom, viewer) {
-  if (atom.clickLabel === undefined
-      || !atom.clickLabel instanceof $3Dmol.Label) {
-    atom.clickLabel = viewer.addLabel(atom.elem + atom.serial, {
-      fontSize : 14,
-      position : {
-        x : atom.x,
-        y : atom.y,
-        z : atom.z
-      },
-      backgroundColor: "black"
-    });
-    atom.clicked = true;
-  }
-
-  //toggle label style
-  else {
-
-    if (atom.clicked) {
-      var newstyle = atom.clickLabel.getStyle();
-      newstyle.backgroundColor = 0x66ccff;
-
-      viewer.setLabelStyle(atom.clickLabel, newstyle);
-      atom.clicked = !atom.clicked;
-    }
-    else {
-      viewer.removeLabel(atom.clickLabel);
-      delete atom.clickLabel;
-      atom.clicked = false;
-    }
-
+var readText = function (input, func) {
+  if (input.files.length > 0) {
+    var file = input.files[0];
+    var reader = new FileReader();
+    reader.onload = function (evt) {
+      func(evt.target.result, file.name);
+    };
+    reader.readAsText(file);
+    $(input).val('');
   }
 };
-var readText = function(input,func) {
-    if(input.files.length > 0) {
-      var file = input.files[0];
-      var reader = new FileReader();
-        reader.onload = function(evt) {
-          func(evt.target.result,file.name);
-        };
-        reader.readAsText(file);
-        $(input).val('');
-    }
-  };
-
-
-
 
 
 async function display3D(protein) {
-	console.log("start display");
-	let element = document.getElementById('container-01');
-	let pdbUri = protein
 
-	glviewer = $3Dmol.createViewer($(element), { backgroundColor: '#669bbc' }); //defaultcolors: $3Dmol.elementColors.rasmol, 
+  let element = document.getElementById('container-01');
+  let pdbUri = protein
 
-	if (pdbUri.startsWith('pdb:')) {
-		glviewer, v = $3Dmol.download(pdbUri, glviewer, { doAssembly: true, noSecondaryStructure: false }, function () {
+  glviewer = $3Dmol.createViewer($(element), { backgroundColor: '#669bbc' }); //defaultcolors: $3Dmol.elementColors.rasmol, 
+
+  if (pdbUri.startsWith('pdb:')) {
+    glviewer, v = $3Dmol.download(pdbUri, glviewer, { doAssembly: true, noSecondaryStructure: false }, function () {
       glviewer.setStyle({}, { cartoon: { color: 'spectrum' } });
-      glviewer.render(); 
-		});
-	} else {
-		jQuery.ajax(pdbUri, {
-			success: function (data) {
-				
-				glviewer.addModel(data, "pdb");                       /* load data */
-				glviewer.setStyle({}, { cartoon: { color: 'spectrum' } });  /* style all atoms */
-				glviewer.zoomTo();                                      /* set camera */
-				glviewer.render();                                      /* render scene */
-			},
-			error: function (hdr, status, err) {
-				console.error("Failed to load PDB " + pdbUri + ": " + err);
-			},
-		})
-	}
+      glviewer.render();
+    });
+  } else {
+    jQuery.ajax(pdbUri, {
+      success: function (data) {
+
+        glviewer.addModel(data, "pdb");                       /* load data */
+        glviewer.setStyle({}, { cartoon: { color: 'spectrum' } });  /* style all atoms */
+        glviewer.zoomTo();                                      /* set camera */
+        glviewer.render();                                      /* render scene */
+      },
+      error: function (hdr, status, err) {
+        console.error("Failed to load PDB " + pdbUri + ": " + err);
+      },
+    })
+  }
 }
 
 
-
-
-
-function download_png() {
-	download = document.getElementById("download_img");
-	var image = document.querySelector("canvas").toDataURL("image/png").replace("image/png", "image/octet-stream");
-	download.setAttribute("href", image);
+function downloadPng() {
+  download = document.getElementById("download_img");
+  var image = document.querySelector("canvas").toDataURL("image/png").replace("image/png", "image/octet-stream");
+  download.setAttribute("href", image);
 }
 
 
+function visualisationButton() {
 
-
-
-function visualisation_nous(){
-
-  let test=
+  let currentView =
     document.querySelector("#molecule2");
-    test.innerHTML = `
+  currentView.innerHTML = `
       <div id="container-molecule2-close">
       <br>
       <br>
@@ -153,7 +126,11 @@ function visualisation_nous(){
       <h2>Atomes & Couleurs</h2>
       <div class="border-drag">
 
-      <input class="buttonbox" type="button" value="Label Cα" onclick="addLabels(glviewer); glviewer.render();">
+      <input class="buttonbox" type="button" value="Label Cα" onclick="addLabels('CA'); glviewer.render();">
+      <input class="buttonbox" type="button" value="Label C-beta" onclick="addLabels('CB'); glviewer.render();">
+      <input class="buttonbox" type="button" value="Remove CA Label" onclick="removeLabels('CA'); glviewer.render();">
+      <input class="buttonbox" type="button" value="Remove CB Label" onclick="removeLabels('CB'); glviewer.render();">
+
       <input class="buttonbox" type="button" value="SS" onclick="colorSS(glviewer);">
       <input class="buttonbox" type="button" value="Spectrum" onclick="colorSpectrum(glviewer);">
       </div>
@@ -163,7 +140,9 @@ function visualisation_nous(){
 
       <input class="buttonbox" type="button" value="Surface1" onclick="surf1 = glviewer.addSurface($3Dmol.SurfaceType.VDW, {}, {hetflag:false,chain:'A'},{hetflag:false,chain:'A'});">
       <input class="buttonbox" type="button" value="Surface2" onclick="surf2 = glviewer.addSurface($3Dmol.SurfaceType.MS, {map:{prop:'partialCharge',scheme:new $3Dmol.Gradient.RWB(-.6,.6)}, opacity:0.85}, {chain:'B'},{chain:'B'});">
-      <input class="buttonbox" type="button" value="RM Surfaces" onclick="glviewer.removeAllSurfaces();">
+      <input class="buttonbox" type="button" value="Surface3" onclick="surf3 = glviewer.addSurface($3Dmol.SurfaceType.SAS, {map:{prop:'partialCharge',scheme:new $3Dmol.Gradient.RWB(-.6,.6)}, opacity:0.85}, {chain:'C'},{chain:'C'});">
+      <input class="buttonbox" type="button" value="Surface4" onclick="surf4 = glviewer.addSurface($3Dmol.SurfaceType.SES, {map:{prop:'partialCharge',scheme:new $3Dmol.Gradient.RWB(-.6,.6)}, opacity:0.85}, {chain:'D'},{chain:'D'});">
+      <input class="buttonbox" type="button" value="Remove all Surfaces" onclick="glviewer.removeAllSurfaces();">
       </div>
 
       <h2>Plus</h2>
@@ -171,7 +150,7 @@ function visualisation_nous(){
 
       <input class="buttonbox" type="button" value="Recenter" onclick="glviewer.zoomTo();">
       <a id="download_img" download="image.png">
-      <button class="buttonbox" type="button" onClick="download_png()">PNG</button></a>
+      <button class="buttonbox" type="button" onClick="downloadPng()">Download in PNG</button></a>
       </div>
 
       </div>
@@ -181,9 +160,9 @@ function visualisation_nous(){
 }
 
 
-function close_button(){
-  let test=
-  document.querySelector("#close_button-id");
+function closeButton() {
+  let test =
+    document.querySelector("#close_button-id");
   test.innerHTML = `
   <div class="container-close">
     <div id ="test-button" onclick="hideThis('test-button'),hideThis('container-01'),hideThis('molecule2'),hideThis('inlineFrameExample')" class="btn-c btn3"></div>
